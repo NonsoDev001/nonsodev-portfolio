3add24b20384a9bbcebd3d955b161aa4fb54a2a1
NonsoDev001 <chukwunonso.freetal@proton.me>
2026-06-26 22:05:06 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import HeroSection from './components/HeroSection'
-import MarqueeSection from './components/MarqueeSection'
 import AboutSection from './components/AboutSection'
 import ServicesSection from './components/ServicesSection'
 import ProjectsSection from './components/ProjectsSection'
@@ -8,7 +7,6 @@ export default function App() {
   return (
     <main style={{ background: '#0C0C0C', overflowX: 'clip' }}>
       <HeroSection />
-      <MarqueeSection />
       <AboutSection />
       <ServicesSection />
       <ProjectsSection />
